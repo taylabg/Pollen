a07ec0925fb01d53de086bd7c2c9000a28db7e83
Tayla <49525732+taylabg@users.noreply.github.com>
2021-10-27 10:57:31 +1100

    Add files via upload

---
+++ Pollen Chat/Pollen/Pollen.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById("welcome").style.opacity = 1;
+    }, 5000);
+	
+	setTimeout(function() {
+		document.getElementById("enter").style.pointerEvents = 'auto';
+        document.getElementById("enter").style.opacity = 1;
+    }, 15000);
+};
+    
